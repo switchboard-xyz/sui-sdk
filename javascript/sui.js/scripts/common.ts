015f39756b9d6715d2f34668867675a35a10a20e
Albert Hermida <alberthermida@gmail.com>
2023-04-30 19:20:39 -0400

    update addrs

--- javascript/sui.js/scripts/common.ts
+++ javascript/sui.js/scripts/common.ts
@@ -1,5 +1,8 @@
 export const SWITCHBOARD_ADDRESS =
-  "0x3ad959d0446d971062f6b93bb88b9f3ffcb7abb5fa6515eb4b9c09b7280d0c7c";
+  "0xe54d5bd1eeb21780315d2c7ae3a5d7c10520b5a694b9f35d7db7f93541b1e9fb";
 export const RPC = "https://fullnode.testnet.sui.io:443";
 
 export const WSS = "wss://fullnode.testnet.sui.io/websocket";
+
+export const TESTNET_QUEUE = `0x7f1b79ef5f575a1bb93d02dcded02525e263f84ac3057e3d36f4441db2ceaaeb`;
+export const TESTNET_ORACLE = `0x60bf3ccfc1c7fc89f4151c963efa5de70a7f515326d3d0336c59a73067513cbb`;
